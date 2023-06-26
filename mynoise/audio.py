8d1367f84a4655b0b19d43cdaf4abcee468c908b
Nat Osaka <dragos240alt@gmail.com>
2023-06-26 11:30:44 -0400

    Working barebones impl

--- mynoise/audio.py
+++ mynoise/audio.py
@@ -1,49 +1,60 @@
-import wave
+from math import ceil
+import os
 
-from pyogg import VorbisFile
-import pyaudio
+from typing import List, Tuple
+from pydub import AudioSegment
+from pydub.effects import normalize
 
 ASSETS_PATH = "./downloaded-assets"
 
 
 class AudioFile:
     """Audio data and loading"""
+    samples: List[AudioSegment]
+
+    def __init__(self) -> None:
+        self.samples = []
 
     def load_file(self, filename: str):
-        self.sample_width = 0
-        self.channels = 0
-        self.sample_rate = 0
-        self.n_frames = 0
-        self.frames = []
         # Determine audio format and parse with corresponding parser
         if filename.endswith(".ogg"):
-            self.parse_vorbis(filename)
+            sample = self.parse_vorbis(filename)
+            self.samples.append(sample)
 
-        # Store result
-        pass
+    def parse_vorbis(self, filename) -> AudioSegment:
+        sample: AudioSegment = AudioSegment.from_ogg(filename)
 
-    def parse_vorbis(self, filename):
-        vf = VorbisFile(filename)
-        self.sample_width = 1
-        self.channels = vf.channels
-        self.sample_rate = vf.frequency
-        self.n_frames = vf.buffer_length
-        self.frames = vf.buffer
-        p = pyaudio.PyAudio()
+        return sample
 
-        stream = p.open(format=p.get_format_from_width(self.sample_width),
-                        channels=self.channels,
-                        rate=self.sample_rate,
-                        output=True)
+    def recursively_overlay(self, secs: int) -> AudioSegment:
+        looped_samples: List[AudioSegment] = []
+        time_in_ms = secs * 1000
+        for sample in self.samples:
+            combined = AudioSegment.empty()
+            n_loops = ceil(time_in_ms / len(sample))
+            # Make enough loops to fill up time_in_ms
+            for _ in range(n_loops):
+                combined = combined + sample
+            looped_samples.append(combined)
+        overlayed = AudioSegment.silent(time_in_ms)
+        for looped_sample in looped_samples:
+            # Overlay the samples
+            overlayed = overlayed.overlay(looped_sample)
+            # Normalize the volume
+            overlayed = normalize(overlayed, headroom=2)
 
-        stream.write(self.frames)
-
-        stream.stop_stream()
-        stream.close()
-
-        p.terminate()
+        if isinstance(overlayed, AudioSegment):
+            return overlayed[:time_in_ms]
+        else:
+            raise Exception("A")
 
 
 if __name__ == '__main__':
     af = AudioFile()
-    af.parse_vorbis("tests/res/sounds/0a.ogg")
+    assets_dir = "downloaded-assets/nowloading"
+    for _, _, filenames in os.walk(assets_dir):
+        for filename in filenames:
+            af.load_file(os.path.join(assets_dir, filename))
+    result: AudioSegment = af.recursively_overlay(120)
+    if isinstance(result, AudioSegment):
+        result.export("out.wav", format='wav')
